9dd2ef4543e9a782505cb73d5a06c49104a2c0f4
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2022-12-16 12:42:07 -0500

    Add numpy implementation of groupby_bins

--- pyglet/transform.py
+++ pyglet/transform.py
@@ -1,4 +1,5 @@
 import numpy as np
+import xarray as xr
 
 def to_spherical(vec, origin):
     """Transform vector components from Cartesian to spherical coordinates
@@ -31,9 +32,9 @@ def to_spherical(vec, origin):
     sin_ph.loc[dict(x=x0,y=y0)] = 0
     cos_ph.loc[dict(x=x0,y=y0)] = 0
     # transform vector components
-    v_r = vx*sin_th*cos_ph + vy*sin_th*sin_ph + vz*cos_th
-    v_th = vx*cos_th*cos_ph + vy*cos_th*sin_ph - vz*sin_th
-    v_ph = -vx*sin_ph + vy*cos_ph
+    v_r = (vx*sin_th*cos_ph + vy*sin_th*sin_ph + vz*cos_th).rename('v_r')
+    v_th = (vx*cos_th*cos_ph + vy*cos_th*sin_ph - vz*sin_th).rename('v_theta')
+    v_ph = (-vx*sin_ph + vy*cos_ph).rename('v_phi')
     # assign spherical coordinates
     v_r.coords['r'] = r
     v_th.coords['r'] = r
@@ -46,3 +47,38 @@ def to_spherical(vec, origin):
     v_ph.coords['ph'] = ph
     vec_sph = (v_r, v_th, v_ph)
     return vec_sph
+
+def groupby_bins(dat, coord, edges, cumulative=False):
+    """Alternative to xr.groupby_bins, which is very slow
+
+    Arguments
+    ---------
+    dat : xarray.DataArray
+        input dataArray
+    coord : str
+        coordinate name along which data is binned
+    edges : array-like
+        bin edges
+    cumulative : bool
+        if True, perform cumulative binning, e.g.,
+          v_r_binned[i] = v_r( edge[0] <= r < edge[i+1] ).mean()
+        to calculate average velocity dispersion within radius r
+
+    Return
+    ------
+    res: xarray.DataArray
+        binned array
+    """
+    dat = dat.transpose('z','y','x')
+    fc = dat[coord].data.flatten() # flattened coordinates
+    fd = dat.data.flatten() # flattened data
+    bin_sum = np.histogram(fc, edges, weights=fd)[0]
+    bin_cnt = np.histogram(fc, edges)[0]
+    if cumulative:
+        bin_sum = np.cumsum(bin_sum)
+        bin_cnt = np.cumsum(bin_cnt)
+    res = bin_sum / bin_cnt
+    # set new coordinates at the bin center
+    centers = 0.5*(edges[1:] + edges[:-1])
+    res = xr.DataArray(data=res, coords={coord:centers}, name=dat.name)
+    return res
